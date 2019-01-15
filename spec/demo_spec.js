237018a7d6fb184106dfebae642dfce91dad6208
jay663 <jay-g6@outlook.com>
2019-01-15 16:10:33 -0500

    end of day2 changes

--- spec/demo_spec.js
+++ spec/demo_spec.js
@@ -1,5 +1,9 @@
 describe('some things', () => {
     it('should behave...', () => {
-        expect(2 + 2).toBe(5);
+        expect(2 + 2).toBe(4);
+    });
+    it('fun with strings', () => {
+        const name = "John O'Conner"
+        const report = 'She said "Do not do it! again';
     });
 }); 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,67 @@
 import './styles.css';
-console.log('Ready to Party');
+import { getRandomColor, getRandomInt, ready } from './utils';
+
+let secretNumber, squares;
+
+function doIt() {
+    // find all the squares.
+    squares = document.querySelectorAll('.square');
+    // on each of them have something happen when they click
+    let squareNumber = 1;    
+    squares.forEach(function(square) {        
+        setRandomColor(square);
+        square.addEventListener('click', handleClick);        
+        square.dataset.number = squareNumber.toString();
+        squareNumber++;
+    });
+
+    // get a random number
+    secretNumber = getRandomInt(1,6);
+    console.log("The secret number is : ", secretNumber);
+
+}
+
+function handleClick(evt) {
+    const currentSquare = this;
+
+    if(parseInt(currentSquare.dataset.number) === secretNumber){                   
+        setWinner(currentSquare);
+        
+        
+        squares.forEach(function (square) {
+            // remove the click handlers 
+            square.removeEventListener('click', handleClick);
+            
+            // and turn non-winners gray
+            if(square !== currentSquare){          
+                square.classList.add("loser");
+                clearLoserColor(square);
+            }
+        });
+
+    }else{
+        clearLoserColor(currentSquare);
+        this.classList.add('loser');
+    }
+    console.log(parseInt(this.dataset.number));
+}
+
+function setRandomColor(div) {
+    console.log(div);
+    div.style.background = getRandomColor();    
+  }
+
+  function clearLoserColor(div) {    
+    div.style.background = "";   
+    div.style.borderColor = "";
+    div.style.borderStyle = "";
+    div.style.borderWidth = ""; 
+  }
+
+  function setWinner(div) {   
+    console.log('You Win!');  
+    div.style.borderColor = '#ffd700';
+    div.style.borderStyle = 'solid';
+    div.style.borderWidth = '1px';    
+  }
+ready(doIt);
